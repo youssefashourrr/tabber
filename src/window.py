043103fa20e872fb59ad5dfa535d33954f3c220f
Youssef Ashour <youssef.ashraf.ashour@gmail.com>
2025-08-09 15:53:15 +0300

    feat: implement window detection function

--- src/window.py
+++ src/window.py
@@ -1,9 +1,9 @@
 class Window:
-    def __init__(self, handle, title, process_name, is_visible):
+    def __init__(self, handle, title, process_id, process_name):
         self.handle = handle
         self.title = title
+        self.process_id = process_id
         self.process_name = process_name
-        self.is_visible = is_visible
 
     def __repr__(self):
-        return f"Window({self.handle}, {self.title}, {self.process_name}, {self.is_visible})"
+        return f"Window({self.handle}, {self.title}, {self.process_id}, {self.process_name})"
--- src/window_manager.py
+++ src/window_manager.py
@@ -5,9 +5,52 @@ import win32con
 
 from window import Window
 
+SYSTEM_PROCESSES = {
+    'dwm.exe',
+    'winlogon.exe',
+    'csrss.exe',
+    'TextInputHost.exe',
+}
 
 class WindowManager:
     def __init__(self):
         self.windows = []
     
+    def _should_include_window(self, handle, process_name):
+        try:
+            ex_style = win32gui.GetWindowLong(handle, win32con.GWL_EXSTYLE)
+            if ex_style & win32con.WS_EX_TOOLWINDOW:
+                return False
+            
+            owner = win32gui.GetWindow(handle, win32con.GW_OWNER)
+            if owner != 0:
+                return False
+            
+            style = win32gui.GetWindowLong(handle, win32con.GWL_STYLE)
+            if not (style & win32con.WS_VISIBLE):
+                return False
+            
+            if process_name in SYSTEM_PROCESSES:
+                return False
+            
+            return True
+        except:
+            return False
     
+    def get_all_windows(self):
+        windows = []
+        def callback(handle, extra):
+            if win32gui.IsWindowVisible(handle):
+                title = win32gui.GetWindowText(handle)
+                if title:
+                    try:
+                        pid = win32process.GetWindowThreadProcessId(handle)[1]
+                        process_name = psutil.Process(pid).name()
+                        if self._should_include_window(handle, process_name):
+                            windows.append(Window(handle, title, pid, process_name))
+                    except (psutil.NoSuchProcess, psutil.AccessDenied):
+                        pass
+            return True
+        win32gui.EnumWindows(callback, None)
+        self.windows = windows
+        return windows
